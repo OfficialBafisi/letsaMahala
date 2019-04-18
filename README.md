649ae786a48f8915ee814ce2ba72d802c7a37029
Katlego Bafisi <katlego@nanoleaf.me>
2019-04-17 20:12:59 -0400

    LetsaMahala v1

---
+++ README.md
@@ -0,0 +1,5 @@
+# wecar
+Car sharing app 
+
+
+Testing first commit
---
+++ src/app/products.ts
@@ -0,0 +1,9 @@
+export class Product  {
+
+	id: number;
+	product_name: string;
+	product_description: string;
+	product_price: number;
+	updated_at: Date;
+
+}
