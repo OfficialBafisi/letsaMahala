48fece6b1336f6391061c4d80f441e0110a0134c
Katlego Bafisi <katlego@nanoleaf.me>
2019-06-26 14:47:12 -0400

    Firebase user auth Fix

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import * as firebase from 'firebase';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AuthenticationService } from './providers/authentication.service'
+firebase.initializeApp(environment.firebase);
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireAuthModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
+  ],
+  declarations: [AppComponent],
+  providers: [InAppBrowser, SplashScreen, StatusBar, AuthenticationService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,37 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+export const environment = {
+  production: false
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#config-web-app -->
+
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAP_guLDjMtP826XCKBDYreo-JI17sqw9w",
+//     authDomain: "letsamahala.firebaseapp.com",
+//     databaseURL: "https://letsamahala.firebaseio.com",
+//     projectId: "letsamahala",
+//     storageBucket: "",
+//     messagingSenderId: "980640081468",
+//     appId: "1:980640081468:web:5234f3c162296519"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+// </script>
